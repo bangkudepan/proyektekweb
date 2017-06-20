349b69bb4659dfd6f4cce00664b89ee3173c1e71
bangkudepan <kamiinfor123@gmail.com>
2017-06-20 20:06:53 +0700

    Add files via upload

---
+++ config/db-config.php
@@ -0,0 +1,13 @@
+<?php
+$db_servername = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name = "proyek";
+
+// Create connection
+$conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
